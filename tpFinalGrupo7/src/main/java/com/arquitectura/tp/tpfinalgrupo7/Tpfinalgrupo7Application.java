870d417c6a701bd57824bad0171ca6ffead85029
Rodrigo Aguilar <rodrigoaguilar96@gmail.com>
2020-10-22 16:41:29 -0300

    add Basic spring and Swagger

---
+++ tpFinalGrupo7/src/main/java/com/arquitectura/tp/tpfinalgrupo7/Tpfinalgrupo7Application.java
@@ -0,0 +1,13 @@
+package com.arquitectura.tp.tpfinalgrupo7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tpfinalgrupo7Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Tpfinalgrupo7Application.class, args);
+  }
+
+}
